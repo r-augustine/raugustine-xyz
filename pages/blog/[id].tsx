ab307c811fce1494a3bc4e67cc9bbd595498f790
Ricardo Augustine <augustineri93@gmail.com>
2023-01-25 23:25:41 -0400

    fix: fixed linting issues

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -33,20 +33,20 @@ const Post = ({ post }: PostProps) => {
     <BlogLayout>
       <h1>{post.title}</h1>
       <ReactMarkdown
-        children={post.content}
         remarkPlugins={[remarkGfm]}
         components={{
           code({ node, inline, className, children, ...props }) {
             const match = /language-(\w+)/.exec(className || "");
             return !inline && match ? (
               <SyntaxHighlighter
-                children={String(children).replace(/\n$/, "")}
                 //@ts-ignore
                 style={dracula}
                 language={match[1]}
                 PreTag="div"
                 {...props}
-              />
+              >
+                {String(children).replace(/\n$/, "")}
+              </SyntaxHighlighter>
             ) : (
               <code className={className} {...props}>
                 {children}
@@ -54,7 +54,9 @@ const Post = ({ post }: PostProps) => {
             );
           },
         }}
-      />
+      >
+        {post.content}
+      </ReactMarkdown>
     </BlogLayout>
   );
 };
